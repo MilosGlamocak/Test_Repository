8d47418a5c8b97b07aae9e756bcead7d896513ed
MilosGlamocak <milosglamocak@gmail.com>
2024-03-14 16:45:40 +0100

    Imported TextField from MUI

--- test/src/Components/AddMovie.jsx
+++ test/src/Components/AddMovie.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import {TextField} from '@mui/material';
+
+
 function AddMovie() {
     console.log(true)
     return (
         <>
+            <TextField id="outlined-basic" label="Movie name" variant="outlined" className="inputMovieName"/>
         </>
     )
 }
